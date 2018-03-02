eab759d2a5691c14b8edf3b96a42418bb4d9281d
youngjoo <hyj@aty.kr>
2018-03-02 11:10:13 +0900

    로그인 - 웹이랑 localStorage공유 막음

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -8,12 +8,10 @@ export const initialApp = () => async dispatch => {
     dispatch({ type: 'auth/LOGIN_ERROR', payload: error });
   });
 
-  console.log(JSON.parse(user));
-
   if (!user) {
     dispatch({ type: 'auth/LOGIN_ERROR', payload: 'no user!' });
   } else {
-    dispatch({ type: 'auth/LOGIN_SUCCESS', payload: JSON.parse(user) });
+    // dispatch({ type: 'auth/LOGIN_SUCCESS', payload: JSON.parse(user) });
   }
 };
 
